ba2c3a0c42c2bffbf9af124bf421b9354f807ff3
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-29 21:13:52 +0530

    jhjk

---
+++ index.js
@@ -0,0 +1,11 @@
+// var score =30;
+// score>=90? score==70? score<=60?console.log("excellent"):console.log("goog"):console.log("need to improve"):console.log("invalid");
+
+var day=sunday;
+day==sunday||saturday?console.log("weekend"):console.log("weekday");
+
+
+
+
+
+
