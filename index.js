98c8d17625eddb5065a22df3646e16761cea9e73
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-29 21:22:36 +0530

    dfg

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // var score =30;
 // score>=90? score==70? score<=60?console.log("excellent"):console.log("goog"):console.log("need to improve"):console.log("invalid");
 
-var day=sunday;
-day==sunday||saturday?console.log("weekend"):console.log("weekday");
-
-
-
-
-
-
+var day = "monday";
+day == "sunday" || "saturday" ? console.log("weekend") : console.log("weekday");
