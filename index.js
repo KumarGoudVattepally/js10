442cdfb4c1ceee67190941819f2bedb40563f665
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-05 10:02:38 +0530

    js10

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-var score =99;
-score>=90? score==70? score<=60?console.log("excellent"):console.log("goog"):console.log("need to improve"):console.log("invalid");
+var score = 98;
+score > 90
+  ? console.log("excellent")
+  : score > 60 && score < 89
+  ? console.log("good")
+  : score < 60
+  ? console.log("need to improve")
+  : console.log("invaild");//excellent
 
-// var day = "monday";
-// day == ("sunday" || "saturday") ? console.log("weekend") : console.log("weekday");
-// var ip = "hello";
+var day = "monday";
+day == ("sunday" || "saturday")
+  ? console.log("weekend")
+  : console.log("weekday");//weekday
 
-// for (i = 0; i <= 4; i++) {
-//   console.log(ip1[i]);
-// }
+var a = "sdjij";
+var b = "";
+for (i = a.length - 1; i >= 0; i--) {
+  b = b + a[i];
+}
+if (a == b) {
+  console.log("is a palindroma");
+} else {
+  console.log("not a palindroma");
+}//not a palindroma
+var ip = "hello";
+var op = "";
+for (i = 0; i <= ip.length - 1; i++) {
+  op = op + ip[i];
+  console.log(op);
+}//h
+// he
+// hel
+// hell
+// hello
